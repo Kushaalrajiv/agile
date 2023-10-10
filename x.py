ddf6e3d623c43ec0923722a8da82d9738362bb38
Kushaal <113694596+Kushaalrajiv@users.noreply.github.com>
2023-10-10 19:20:26 +0530

    Update x.py

--- x.py
+++ x.py
@@ -1,29 +1,6 @@
-rr=""
-started=False
-while True:
-    rr=input("> ")
-    if rr=="help":
-        print('''
-    start-to start the car
-    stop-to stop the car
-    quit-to exit
-    ''')
-    elif rr=="start":
-        if started==False:
-            
-            print("car started...ready to go")
-            started=True
-        else:
-            print("Car already started")
-    elif rr=="stop":
-        if started==True:
-            print("car stopped")
-            started=False
-        else:
-            print("already stopped")
-    elif rr=="quit":
-        break
-
-    else:
-        print("sorry i dont understand that")
-
+numb=[1,1,1,1,5]
+for n in numb:
+    output=""
+    for t in range(n):
+        output+="*"
+    print(output)
